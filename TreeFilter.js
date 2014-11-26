bbd40cbaf02eb0b14dcb8e1edc81cb65429c9f84
nichoth <nichoth@gmail.com>
2014-11-26 08:23:17 -0800

    Use globals, decouple from catalog.

--- TreeFilter.js
+++ TreeFilter.js
@@ -1,16 +1,14 @@
 /**
- * FilterModule.js
+ * TreeFilter.js
  * Container for a binary tree of FilterFacets. Maps events to methods.
  */
-
-
-$.bookmark.FilterModule = function(getFn) {
-  if (!catalog) {throw new Error ('need more parameters');}
+TreeFilter = function(getFn) {
+  if (!getFn) {throw new Error ('need more parameters');}
   this.getFn = getFn;
   this.root = false;
 };
 
-_($.bookmark.FilterModule.prototype).extend(Backbone.Events, {
+_(TreeFilter.prototype).extend(Backbone.Events, {
 
   /**
    * Add a facet to the filter tree.
@@ -21,9 +19,9 @@ _($.bookmark.FilterModule.prototype).extend(Backbone.Events, {
    */
   addQuery: function (query, op) {
     if (this.root) {
-      var newRoot = new $.bookmark.FilterFacet(this.getFn, {op: op || 'intersect'});
+      var newRoot = new FilterFacet(this.getFn, {op: op || 'intersect'});
       newRoot.left = this.root;
-      newRoot.right = new $.bookmark.FilterFacet(this.catalog, {query: query});
+      newRoot.right = new FilterFacet(this.catalog, {query: query});
       this.root = newRoot;
     } else {
       this.root = new $.bookmark.FilterFacet(this.catalog, {query: query});
@@ -36,11 +34,10 @@ _($.bookmark.FilterModule.prototype).extend(Backbone.Events, {
 
   hasPossibilites: function (query) {
     if (this.root) {
-      return this.root.intersect( this.catalog.get(query) ).length > 0;
+      return this.root.intersect( getFn(query) ).length > 0;
     } else {
       return true;
     }
-
   },
 
   onDestroy: function(survivor) {
@@ -56,7 +53,117 @@ _($.bookmark.FilterModule.prototype).extend(Backbone.Events, {
   },
 
   results: function() {
-    return (this.root) ? this.root.results() : this.catalog.nodeList.models;
+    return (this.root) ? this.root.results() : this.getFn();
   }
+
+});
+
+
+/**
+ * FilterFacet
+ * A node in a binary tree of queries.
+ * This caches the results of a query, or caches the results of its op applied
+ * to its childrens' results.
+ *
+ * The cached results are a set of BB Models.
+ *
+ * Listens to cached models or children for attr changes that affect this's
+ * results.
+ */
+FilterFacet = function(getFn, opts) {
+
+  this.getFn = getFn;
+
+  this.op = opts.op || false;
+  this.query = opts.query || false;
+  this.resultSet = false;
+  this.left = opts.left || false;
+  this.right = opts.right || false;
+
+};
+
+_(FilterFacet.prototype).extend(Backbone.Events, {
+  results: function() {
+    if (this.resultSet) { return this.resultSet; }
+    else {
+      if (this.left) {
+        // is branch, apply op to child results
+        this.resultSet = this.left[this.op]( this.right.results() );
+        this.listenTo(this.left, 'destroy', this.onLeftDestroy);
+        this.listenTo(this.left, 'change', this.onChildChange);
+        this.listenTo(this.right, 'destroy', this.onRightDestroy);
+        this.listenTo(this.right, 'change', this.onChildChange);
+      } else {
+        // is leaf, evaluate query
+        this.resultSet = getFn(this.query);
+        // re-cache when the query changes
+        this.listenTo(this.query.field, 'some-event', this.results());
+        this.listenTo(this.query.value, 'some-event', this.results());
+      }
+      return this.resultSet;
+    }
+  },
+
+  toString: function() {
+    var val;
+    if (this.query) {
+      val = this.query.toString();
+    } else {
+      val = this.op;
+    }
+    return val;
+  },
+
+  onLeftDestroy: function (survivor) {
+    if (survivor) {
+      this.stopListening(this.left);
+      this.left = survivor;
+      this.listenTo(this.left, 'destroy', this.onLeftDestroy);
+      this.refresh();
+    } else {
+      this.destroy(this.right);
+    }
+  },
+
+  onRightDestroy: function (survivor) {
+    if (survivor) {
+      this.stopListening(this.right);
+      this.right = survivor;
+      this.listenTo(this.right, 'destroy', this.onRightDestroy);
+      this.refresh();
+    } else {
+      survivor = this.left;
+      this.destroy(survivor);
+    }
+  },
+
+  // re-cache results
+  // to-do:
+  //    only trigger change if the new results are different
+  refresh: function () {
+    if (this.left) {
+      this.resultSet = this.left[this.op](this.right);
+    } else {
+      this.resultSet = getFn(this.query);
+    }
+    this.trigger('change');
+  },
+
+  // re-cache the result set
+  onChildChange: function (child) {
+    this.refresh();
+  },
+
+  destroy: function (survivor) {
+    this.trigger('destroy', survivor);
+  },
+
+  // ops:
+  intersect: function(arr) {
+    return _( this.results() ).intersection(arr);
+  },
+  union: function() {},
+
+  difference: function() {},
 
 });
